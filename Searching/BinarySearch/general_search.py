6eda47321f40ae82f9dd31943f7a1683b86ab9f7
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-07-22 08:27:47 +0000

    find max min simulaneouslt

--- Searching/BinarySearch/general_search.py
+++ Searching/BinarySearch/general_search.py
@@ -1,5 +1,6 @@
 # Search in2D sorted array
-
+import collections
+import sys
 A = [[-1,2,4,4,6],
      [1,5,5,9,21],
      [3,6,6,9,22],
@@ -34,6 +35,46 @@ def search_2D_optimized(A,x):
             col -= 1
     return False
 
-x = search_2D_optimized(A,7)
-y = search_2D_optimized(A,9)
-print(x,y)
+# x = search_2D_optimized(A,7)
+# y = search_2D_optimized(A,9)
+# print(x,y)
+
+
+A = [3,2,5,1,2,4]
+
+def find_min_max_simutanously_brute(A):
+    mini = A[0]
+    maxi = A[0]
+    for i, v in enumerate(A):
+        if A[i] < mini:
+            mini = A[i]
+        elif A[i] >= maxi:
+            maxi = A[i]
+    # print(mini, maxi)
+
+MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))    
+def find_min_max_simutanously(A):
+    def min_max(a,b):
+        return MinMax(a,b) if a < b else MinMax(b, a)
+    
+    if len(A) <= 1:
+        return MinMax(A[0], A[0])
+    
+    global_min_max = min_max(A[0], A[1])
+    for i in range(2, len(A)-1, 2):
+        local_min_max = min_max(A[i], A[i+1])
+        global_min_max = MinMax(
+            min(global_min_max.smallest, local_min_max.smallest),
+            max(global_min_max.largest, local_min_max.largest)
+        )
+
+    if len(A) % 2:
+        global_min_max = MinMax(
+            min(global_min_max.smallest, A[-1]),
+            max(global_min_max.largest, A[-1])
+        )
+    return global_min_max
+   
+
+ans = find_min_max_simutanously(A)
+print(ans)
