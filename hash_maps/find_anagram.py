93d2f1ab20e2ac7d30b8df184d260fa183bd1319
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-07-30 11:43:12 +0000

    add find pallindrome

--- hash_maps/find_anagram.py
+++ hash_maps/find_anagram.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+x = ["debitcard", "elvis", "silent", 'badcredit', 'lives', 'freedom',
+"listen","levis","money"]
+
 def find_anagram(dictionary):
     sorted_string_to_anagrams = defaultdict(list)
     for s in dictionary:
@@ -8,4 +11,44 @@ def find_anagram(dictionary):
     return [
     group for group in sorted_string_to_anagrams.values() if len(group) >= 2
     ]
+
+ans = find_anagram(x)
+
+"""A palindrome is a string that can reas the same forwards  
+and backwards eg level , rotator and foobaraboof"""
+from collections import Counter
+def check_palindrome_permutation(s):
+    palindrome_count = [ v for v in Counter(s).values()]
+    check_odd = [ v % 2 for v in palindrome_count]
+    odd_sum = sum(check_odd) <= 1
+    print(odd_sum)
+
+check_palindrome_permutation("rotator")
+print(Counter("rotator"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
